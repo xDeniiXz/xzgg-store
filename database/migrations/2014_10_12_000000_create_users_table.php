b356565ffaab10891954dc43940412227bde9143
DeniiXz <mdeniyulio@gmail.com>
2026-02-05 19:41:10 +0700

    Set default role to 'operator' in users table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('role', ['super_admin', 'admin', 'operator']);
+            $table->enum('role', ['super_admin', 'admin', 'operator'])
+                ->default('operator');
             $table->rememberToken();
             $table->timestamps();
         });
